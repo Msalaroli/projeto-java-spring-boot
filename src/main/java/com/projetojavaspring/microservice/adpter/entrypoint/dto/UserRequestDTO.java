17ac2ddc29c34003eaa9845bc49590902d68bef5
Msalaroli <saaroli1998@hotmail.com>
2025-06-04 16:53:24 -0300

    chore: change class to record

--- src/main/java/com/projetojavaspring/microservice/adpter/entrypoint/dto/UserRequestDTO.java
+++ src/main/java/com/projetojavaspring/microservice/adpter/entrypoint/dto/UserRequestDTO.java
@@ -1,16 +1,13 @@
 package com.projetojavaspring.microservice.adpter.entrypoint.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
-@Data
-public class UserRequestDTO { //todo: refatorar para record
-    @NotNull
-    private String username;
+public record UserRequestDTO(
+        @NotNull
+        String username,
 
-    @NotNull
-    private String email;
+        @NotNull
+        String email,
 
-    @NotNull
-    private String password;
-}
+        @NotNull
+        String password) {}
--- src/main/java/com/projetojavaspring/microservice/adpter/entrypoint/dto/UserResponseDTO.java
+++ src/main/java/com/projetojavaspring/microservice/adpter/entrypoint/dto/UserResponseDTO.java
@@ -1,16 +1,14 @@
 package com.projetojavaspring.microservice.adpter.entrypoint.dto;
 
-import lombok.Data;
-
 import java.time.LocalDateTime;
 
-@Data
-public class UserResponseDTO { //todo: refatorar para record
-    private Long id;
-    private String username;
-    private String email;
-    private String fullName;
-    private String bio;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-}
+public record UserResponseDTO(
+        Long id,
+        String username,
+        String email,
+        String fullName,
+        String bio,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {}
+
